307459ee0a301a68936f544958935bda83a6f04e
Viktor Fonic <viktor.fonic@gmail.com>
2019-10-25 12:46:01 +0200

    Create graphql error

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -4,10 +4,10 @@ module Types
     # They will be entry points for queries on your schema.
 
     # TODO: remove me
-    field :test_field, String, null: false,
+    field :test_field, [String], null: false,
       description: "An example field added by the generator"
     def test_field
-      "Hello World!"
+      String
     end
   end
 end
